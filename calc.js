174518ae499a02a313b12c8570dec546e34a6c64
aswanthns <aswanth9745730594@gmail.com>
2024-03-21 11:56:07 +0530

    first

---
+++ calc.js
@@ -0,0 +1,12 @@
+function displayData(n) {
+    result.value+=n
+}
+function allClear() {
+    result.value=" " 
+}
+function delOne() {
+    result.value = result.value.slice(0,-1)
+}
+function findOut(){
+    result.value=eval(result.value)
+}
